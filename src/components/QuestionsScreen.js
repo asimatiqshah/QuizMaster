17e305619e51e23928294a684eeb50f28b6e74b9
asimshah <shah.asimatiq@gmail.com>
2024-07-14 10:16:31 +0500

    Question Screen database data render only

--- src/components/QuestionsScreen.js
+++ src/components/QuestionsScreen.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import {
   Button,
+  FlatList,
   Image,
   ImageBackground,
   StyleSheet,
@@ -18,7 +19,7 @@ const QuestionsScreen = ({ route }) => {
 
   useEffect(() => {
     fetchData();
-  },[])
+  }, [])
 
 
   const fetchData = async () => {
@@ -40,26 +41,88 @@ const QuestionsScreen = ({ route }) => {
     return result;
   }
 
-  const nextQuestions=()=>{
-    if((currentPage + 1) < question.length){
+  const nextQuestions = () => {
+    if ((currentPage + 1) < question.length) {
       const next = currentPage + 1;
       setCurrentPage(next);
     }
   }
 
+  const prevQuestions = ()=>{
+    if((currentPage - 1) >= 0){
+      const prev = currentPage - 1;
+      setCurrentPage(prev);
+    }
+  }
+
+  const renderItem = ({ item }) => {
+    return (
+      <View>
+        <View style={{ paddingHorizontal: 25, marginBottom: 20, marginTop: 30 }}>
+          <Text style={[styles.heading2, { color: '#37E9BB' }]}>0{currentPage+1}/0{question.length}</Text>
+          <Text style={styles.heading1}>{item.question}</Text>
+        </View>
+        <Image
+          source={require('../images/tajmahal.jpg')}
+          style={{
+            width: 304,
+            height: 214,
+            borderRadius: 10,
+            borderWidth: 2,
+            alignSelf: 'center',
+            borderColor: '#ffff',
+          }}
+        />
+        {/* Bullets */}
+        <View
+          style={{ justifyContent: 'flex-start', alignItems: 'flex-start', marginTop: 20 }}>
+          {
+            item.options.map((record,index) => {
+              return (
+                <View key={index} style={{ flexDirection: 'row', paddingVertical: 10 }}>
+                  <Image
+                    source={require('../images/num_01.png')}
+                    style={{ width: 50, height: 50 }}
+                  />
+                  <Text style={[styles.heading2, { paddingLeft: 20,color:'#37E9BB' }]}>
+                    {record}
+                  </Text>
+                </View>
+              )
+            })
+          }
+        </View>
+      </View>
+    )
+  }
+
   return (
-    <View>
-      {
-        question && getCurrentQuestions().map((record, index) => {
-          return (
-              <View key={index}>
-                <Text>{record.question}</Text>
-              </View>
-          )
-        })
-      }
-      <Button title='Next' onPress={()=>nextQuestions()} />
-    </View>
+    <>
+      <ImageBackground
+        source={require('../images/fullimage.jpg')}
+        resizeMode="cover"
+        style={styles.bg_full}>
+        {
+          <FlatList
+            data={getCurrentQuestions()}
+            renderItem={renderItem}
+          />
+        }
+        {/* Buttons */}
+        <View style={{ position: 'absolute', bottom: 0, width: '100%' }}>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            <TouchableOpacity disabled={currentPage == 0} onPress={()=>prevQuestions()}  style={{}}>
+              <Image source={require('../images/prev.png')} style={{opacity:( currentPage == 0 )?0.2:1}} />
+            </TouchableOpacity>
+            <TouchableOpacity disabled={(currentPage + 1) >= question.length} onPress={()=>nextQuestions()}>
+              <Image source={require('../images/next.png')} style={{opacity: ((currentPage + 1) >= question.length)? 0.2 : 1}} />
+            </TouchableOpacity>
+          </View>
+        </View>
+
+      </ImageBackground>
+    </>
+
   );
 };
 export default QuestionsScreen;
